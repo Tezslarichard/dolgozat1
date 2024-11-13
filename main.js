2d156f1a49bc385d61af96ad860c6d655e13a13d
Tezslarichard <tezslarichard@gmail.com>
2024-11-13 13:02:10 +0100

    tbodym meg nincs commitolva de haladok ha minden igaz

--- main.js
+++ main.js
@@ -1,41 +1,32 @@
 let array = [
     {
-    uralkod : "I. István",
-    esemeny : "Koronázás",
-    evszam : "1000",
+    uralkodo_nev : "I. István",
+    esemeny1 : "Koronázás",
+    evszam1 : "1000",
+    esemeny2 : "Pannonhalmi apátság megalapítása",
+    evszam2 : "1001",
     },
 
     {
-    esemeny : "Pannonhalmi apátság megalapítása",
-    evszam : "1001",
+    uralkodo_nev : "IV. Béla",
+    esemeny1 : "tatárjárás",
+    evszam1 : "1241-1242",
     },
 
     {
-    uralkod : "IV. Béla",
-    esemeny : "tatárjárás",
-    evszam : "1241-1242",
+    uralkodo_nev : "Mátyás király",
+    esemeny1 : "Bécs elfoglalása",
+    evszam1 : "1485",
+    esemeny2 : "Kenyérmezei csata",
+    evszam2 : "1479",
     },
 
     {
-    uralkod : "Mátyás király",
-    esemeny : "Bécs elfoglalása",
-    evszam : "1485",
-    },
-
-    {
-    esemeny : "Kenyérmezei csata",
-    evszam : "1479",
-    },
-
-    {
-    uralkod : "II. Rákóczi Ferenc",
-    esemeny : "A szabadságharc kezdete",
-    evszam : "1703",
-    },
-
-    {
-    esemeny : "A szabadságharc vége",
-    evszam : "1711",
+    uralkodo_nev : "II. Rákóczi Ferenc",
+    esemeny1 : "A szabadságharc kezdete",
+    evszam1 : "1703",
+    esemeny2 : "A szabadságharc vége",
+    evszam2 : "1711",
     }
 
 
@@ -43,11 +34,65 @@ let array = [
 ]
 
 const table = document.createElement('table');
+    document.body.appendChild(table);
 const tbody = document.createElement('tbody');
+
 const thead = document.createElement('thead');
+    table.appendChild(thead);
 const tr = document.createElement('tr');
+    thead.appendChild(tr);
+
+const uralkodo = document.createElement('tr');  
+    uralkodo.innerHTML = 'Úralkodó'
+    tr.appendChild(uralkodo)
+
+const esemeny = document.createElement('tr');
+    esemeny.innerHTML = 'Esemény';
+    tr.appendChild(esemeny);
+
+const evszam = document.createElement('tr');
+    evszam.innerHTML = 'Évszám';
+    tr.appendChild(evszam);
 
 
-document.body.appendChild(table);
-table.appendChild(thead);
-thead.appendChild(tr);
+
+    const form = document.getElementById('form');
+form.addEventListener('submit' ,function(e){
+    const uralkodo = document.getElementById('uralkodo_nev');
+    const esemeny1 = document.getElementById('esemeny1');
+    const evszam1 = document.getElementById('evszam1');
+    const esemeny2 = document.getElementById('esemeny2');
+    const evszam2 = document.getElementById('evszam2');
+
+
+    e.preventDefault();
+    const uralkodo_nev_value = uralkodo.value;
+    const esemeny1_value = esemeny1.value;
+    const evszam1_value = evszam1.value;
+    const esemeny2_value = esemeny2.value;
+    const evszam2_value = evszam2.value;
+
+
+    const adatok = [
+        {
+            uralkodo : uralkodo_nev_value ,
+            esemeny1 : esemeny1_value,
+            evszam1 : evszam1_value,
+            esemeny2 : esemeny2_value,
+            evszam2 : evszam2_value,
+        }
+    ]
+
+    array.push(adatok);
+    form.reset();
+})
+
+    function irdki(){
+
+}
+
+
+
+
+
+
